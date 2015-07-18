2a14cfe8fcf38a3a591520a7a37927c213406e37
mknudsen01 <mknudsen01@gmail.com>
2015-07-18 12:41:02 -0700

    Test post request

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ app.get('/', function(req, res) {
   res.send('Look, I\'m working');
 })
 
+app.post('/api/test', function (req, res) {
+  console.log('POST request to the homepage');
+  res.send('POST request to the homepage');
+});
+
 var server = app.listen(process.env.PORT || 3000, function() {
   var host = server.address().address;
   var port = server.address().port;
