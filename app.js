e0c1543034125c96456c344b8b006d6446c07e18
mknudsen01 <mknudsen01@gmail.com>
2015-07-18 16:18:29 -0700

    Add basic socket connection

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var app = express();
+var server =require('http').createServer(app);
+
+var io = require('socket.io')(server);
 
 app.get('/', function(req, res) {
-  res.send('Look, I\'m working');
+  console.log('GET request to homepage');
+  res.sendfile(__dirname + '/index.html');
+})
+
+io.on('connection', function(socket) {
+  console.log('user connected');
 })
 
 app.post('/api/test', function (req, res) {
@@ -10,9 +18,6 @@ app.post('/api/test', function (req, res) {
   res.send('POST request to the homepage');
 });
 
-var server = app.listen(process.env.PORT || 3000, function() {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('App listening at http://%s:%s', host, port);
+var server = server.listen(process.env.PORT || 3000, function() {
+  console.log('App listening at *3000');
 })
